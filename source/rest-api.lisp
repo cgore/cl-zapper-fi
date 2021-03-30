68a8e1fb417edae3e0a97ce01060397b6d493c4d
Christopher Mark Gore <cgore@cgore.com>
2021-03-30 14:13:18 -0500

    First API call, just getting gas price

---
+++ source/rest-api.lisp
@@ -0,0 +1,18 @@
+(defpackage #:zapper-fi/rest-api
+  (:use :common-lisp))
+(in-package :zapper-fi/rest-api)
+
+(defvar *api* "http://api.zapper.fi/v1")
+
+(defun build-url (&rest url-components)
+  (apply #'concatenate 'string *api* url-components))
+
+(defun http-get-json (&rest url-components)
+  "Make an HTTP GET call to the Zapper.fi API, expecting JSON back, and parse."
+  (let ((yason:*parse-json-arrays-as-vectors*   t)
+        (yason:*parse-json-booleans-as-symbols* t))
+    (yason:parse (dex:get (apply #'build-url url-components)))))
+
+(defun get-gas-price ()
+  "Get the gas price from the API."
+  (http-get-json "/gas-price"))
